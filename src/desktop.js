7b2a8a4b993b7a302b1e22a44b8cfd5fb250dedd
0mg <millig@outlook.jp>
2018-03-30 18:44:03 +0900

    dez: <meta viewport> for mobile

--- src/desktop.js
+++ src/desktop.js
@@ -3,3 +3,11 @@ document.createElement("section");
 document.createElement("article");
 document.createElement("aside");
 document.createElement("nav");
+
+// for Mobile
+addEventListener("load", function() {
+  var e = document.createElement("meta");
+  e.name = "viewport";
+  e.content = "width=device-width";
+  document.head.appendChild(e);
+});
